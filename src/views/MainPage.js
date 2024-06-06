46e415077ec0f65cae2c6b38f2315d6af976bf68
TobarasChristopher <tobaraschristopher@yahoo.ie>
2024-06-06 12:01:10 +0100

    init commit

---
+++ src/views/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/MainPage.css';
+
+const MainPage = () => {
+    return (
+        <div className="main-page">
+            <h1>Welcome to the Animated Calendar App</h1>
+            <p>This is the main landing page.</p>
+        </div>
+    );
+};
+
+export default MainPage;
